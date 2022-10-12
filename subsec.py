a891fd89c0f5e324fef33d5e0296ae884249d694
jmRiveral <60269766+jmRiveral@users.noreply.github.com>
2022-10-11 23:57:37 -0500

    ]?'???

--- subsec.py
+++ subsec.py
@@ -1,16 +1,47 @@
 from ast import For
 import sys
+from unittest import skip
 
+
+def max_str( m, X ,Y ):
+    counter=0  
+    #cambio en letras para maximizar sub str
+    for i in range (0,m):
+        
+        if ((i%2)==0):
+            for j in reversed(range(0,len(X))):
+                if X[j]!=Y[0]:
+                    X[j]=Y[0]
+                
+               
+               
+        if ((i%2)==1):
+            for j in range(0, len(X)):
+                if X[j]!=Y[0]:
+                    X[j]=Y[0]
+               
+           
+          
+    for i in range(0, len(X)):
+        
+        if X[i]==Y[0]:
+            interval=X[i+1:len(X)]
+            counter+=interval.count(Y[1])
+            
+    return X
+    
 def main():
     linea = sys.stdin.readline()
     casos = int(linea)
     linea = sys.stdin.readline()
     for i in range(0,casos):
-        datastr =linea.split( )
-        X = datastr[0] #string grande
-        Y = datastr[1] #sub string
-        m = datastr[2] #movimientos max?
-        print(datastr[1])
+        
+        datastr =linea.split()
+        x=list(datastr[0])
+        y=list(datastr[1])
+        m=int(datastr[2])
+        result= max_str(m,x,y)
+        print(result)
         linea = sys.stdin.readline()
 
 main()
