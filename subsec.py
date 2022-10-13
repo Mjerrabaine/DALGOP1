1b00c0b2ff2736e6d4f6c8dea51a06804d080112
jmRiveral <60269766+jmRiveral@users.noreply.github.com>
2022-10-12 23:22:46 -0500

    76%

--- subsec.py
+++ subsec.py
@@ -1,5 +1,5 @@
-from ast import For
 import sys
+import time
 from unittest import skip
 
 
@@ -9,18 +9,9 @@ def max_str( m, X ,Y ):
     #cambio en letras para maximizar sub str
     for i in range (0,m):
         b=True
-        #par
-        if ((i%2)==0):
-            j= len(X)-1
-            while b and j > 0:
-                if X[j]!=Y[1]:
-                    X[j]=Y[1]
-                    b=False
-                j-=1
-                
-               
-        #imapr 
-        if ((i%2)==1):
+        c1=X.count(Y[0])
+        c2=X.count(Y[1])
+        if (c1<c2):
             j=0
             while b and j < len(X) :
                 if X[j]!=Y[0]:
@@ -28,9 +19,23 @@ def max_str( m, X ,Y ):
                     b=False
                 j+=1
 
+        else:
+            j= len(X)-1
+            while b and j > 0:
+                if X[j]!=Y[1]:
+                    X[j]=Y[1]
+                    b=False
+                j-=1
+        
+       
+           
+            
+           
+       
+
                
            
-          
+    #contador
     for i in range(0, len(X)):
         
         if X[i]==Y[0]:
@@ -38,11 +43,26 @@ def max_str( m, X ,Y ):
             counter+=interval.count(Y[1])
             
     return counter
-    
+
+
+def porcentaje_correcto(comp):
+    prueba = open('P1_cases.out','r')
+    s=[]
+    for i in range(0,len(comp)):
+        temp=int(prueba.readline())
+        s.append(temp)
+    count=0
+    for i in range(0,len(comp)):
+        if (s[i]==comp[i]):
+            count+=1
+    return str(count*100 / len(comp))+"%"
+
+
 def main():
     linea = sys.stdin.readline()
     casos = int(linea)
     linea = sys.stdin.readline()
+    comp= []
     for i in range(0,casos):
         
         datastr =linea.split()
@@ -50,7 +70,11 @@ def main():
         y=list(datastr[1])
         m=int(datastr[2])
         result= max_str(m,x,y)
+        comp.append(result)
         print(result)
         linea = sys.stdin.readline()
+    print(porcentaje_correcto(comp))
 
+start_time=time.time()
 main()
+print("--- %s tiempo secgundos ---" % (time.time() - start_time))
